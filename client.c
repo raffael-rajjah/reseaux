b20f18340110dda16d1dd0cf66b1a891a8d895b6
Raffael Dwyyana <raffael.dwyyana@univ-tlse3.fr>
2024-05-21 15:57:06 +0200

    init

---
+++ client.c
@@ -0,0 +1,154 @@
+/* =================================================================== */
+// Progrmame Client à destination d'un joueur qui doit deviner la case
+// du trésor. Après chaque coup le résultat retourné par le serveur est
+// affiché. Le coup consite en une abcsisse et une ordonnée (x, y).
+/* =================================================================== */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define N 10
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define MAGENTA "\033[35m"
+
+/* ====================================================================== */
+/*                  Affichage du jeu en mode texte brut                   */
+/* ====================================================================== */
+void afficher_jeu(int jeu[N][N], int res, int points, int coups) {
+
+    printf("\n************ TROUVEZ LE TRESOR ! ************\n");
+    printf("    ");
+    for (int i=0; i<10; i++)
+        printf("  %d ", i+1);
+    printf("\n    -----------------------------------------\n");
+    for (int i=0; i<10; i++){
+        printf("%2d  ", i+1);
+        for (int j=0; j<10; j++) {
+            printf("|");
+            switch (jeu[i][j]) {
+                case -1:
+                    printf(" 0 ");
+                    break;
+                case 0:
+                    printf(GREEN " T " RESET);
+                    break;
+                case 1:
+                    printf(YELLOW " %d " RESET, jeu[i][j]);
+                    break;
+                case 2:
+                    printf(RED " %d " RESET, jeu[i][j]);
+                    break;
+                case 3:
+                    printf(MAGENTA " %d " RESET, jeu[i][j]);
+                    break;
+            }
+        }
+        printf("|\n");
+    }
+    printf("    -----------------------------------------\n");
+    printf("Pts dernier coup %d | Pts total %d | Nb coups %d\n", res, points, coups);
+}
+
+
+#define SERVEURNAME "127.0.0.1"
+int to_server_socket = -1;
+
+
+/* ====================================================================== */
+/*                    Fonction principale                                 */
+/* ====================================================================== */
+int main(int argc, char **argv) {
+
+    int jeu[N][N];
+    int lig, col;
+    int res = -1, points = 0, coups = 0;
+
+    /* Init args */
+
+    char *server_name = SERVEURNAME;
+    struct sockaddr_in serverSockAddr;
+    struct hostent *serverHostEnt;
+    long hostAddr;
+    long status;
+    char buffer[512]; 
+
+    /* Init jeu */
+    for (int i=0; i<N; i++)
+        for (int j=0; j<N; j++)
+            jeu[i][j] = -1;
+
+
+    /* Init caracteristiques serveur distant (struct sockaddr_in) */
+    bzero(&serverSockAddr,sizeof(serverSockAddr));
+    hostAddr = inet_addr(SERVEURNAME);
+    if ( (long)hostAddr != (long)-1)
+        bcopy(&hostAddr,&serverSockAddr.sin_addr,sizeof(hostAddr));
+
+    serverSockAddr.sin_port = htons(5555);
+    serverSockAddr.sin_family = AF_INET;
+
+    /* creation de la socket */
+    if ( (to_server_socket = socket(AF_INET,SOCK_STREAM,0)) < 0)
+    {
+        printf("Probleme de la creation socket client\n");
+        exit(0);
+    }
+
+    /* Etablissement connexion TCP avec process serveur distant */
+    if(connect( to_server_socket,
+                (struct sockaddr *)&serverSockAddr,
+                sizeof(serverSockAddr)) < 0 )
+    {
+        printf("Probleme de la demande de connection\n");
+        exit(0);
+    }
+
+
+    /* Tentatives du joueur : stoppe quand tresor trouvé */
+    do {
+        /* Construction requête (serialisation en chaines de caractères) */
+        afficher_jeu(jeu, res, points, coups);
+        printf("\nEntrer le numéro de ligne : ");
+        scanf("%d", &lig);
+        printf("Entrer le numéro de colonne : ");
+        scanf("%d", &col);
+        sprintf(buffer, "%d %d", lig, col);
+
+
+        /* Envoi de la requête au serveur (send) */
+        send(to_server_socket, buffer, 512, 0);
+
+        
+        /* Réception du resultat du coup (recv) */
+        recv(to_server_socket, buffer, 512, 0);
+
+        /* Deserialisation du résultat en un entier */
+        sscanf(buffer, "%d", &res);
+        
+
+        /* Mise à jour */
+        if (lig>=1 && lig<=N && col>=1 && col<=N)
+            jeu[lig-1][col-1] = res;
+        points += res;
+        coups++;
+
+    } while (res);
+
+    /* Fermeture connexion TCP */
+    shutdown(to_server_socket,2);
+    close(to_server_socket);
+
+    /* Terminaison du jeu : le joueur a trouvé le tresor */
+    afficher_jeu(jeu, res, points, coups);
+    printf("\nBRAVO : trésor trouvé en %d essai(s) avec %d point(s)"
+            " au total !\n\n", coups, points);
+    return 0;
+}
---
+++ serveur1.c
@@ -0,0 +1,111 @@
+/* =================================================================== */
+// Progrmame Serveur qui calcule le résultat d'un coup joué à partir
+// des coordonnées reçues de la part d'un client "joueur".
+// Version ITERATIVE : 1 seul client/joueur à la fois
+/* =================================================================== */
+
+#include <stdio.h>
+
+/* =================================================================== */
+/* FONCTION PRINCIPALE : SERVEUR ITERATIF                              */
+/* =================================================================== */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include "tresor.h"
+#include <string.h>
+
+
+char buffer[512];
+int ma_socket;
+
+int main ( void )
+{
+    int client_socket;
+    struct sockaddr_in mon_address, client_address;
+    int mon_address_longueur, lg;
+    int x_tresor = 4, y_tresor = 5;
+    int xp, yp;
+
+
+    //     srand(time(NULL));
+    //     x_tresor = 1 + rand()%N;
+    //     y_tresor = 1 + rand()%N;
+
+
+    int res;
+
+    bzero(&mon_address,sizeof(mon_address)); 
+    mon_address.sin_port = htons(5555); 
+    mon_address.sin_family = AF_INET;
+    mon_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+    /* creation de socket */
+    if ((ma_socket = socket(AF_INET,SOCK_STREAM,0))== -1)
+    {
+        printf("Probleme avec la creation\n");
+        return 0;
+    }
+
+    /* bind serveur - socket */
+    bind(ma_socket,(struct sockaddr *)&mon_address,sizeof(mon_address));
+    
+
+    listen(ma_socket,5);
+
+    mon_address_longueur = sizeof(client_address);
+    client_socket = accept(ma_socket,
+                          (struct sockaddr *)&client_address,
+                           &mon_address_longueur);
+
+
+        while(1){
+
+            if(recv(client_socket, buffer, 512, 0) == -1){
+                printf("ERROR recv");
+
+                return 0;
+            }
+
+            if(strcmp(buffer, "0") == 0){ // si le client est déconnecté, fermer le socket et attendre un nouveau client
+                
+                memset(buffer, 0, sizeof(buffer));
+
+                shutdown(client_socket,2);
+                close(client_socket);
+
+                printf("client disconnected");
+
+                client_socket = accept(ma_socket,
+                                (struct sockaddr *)&client_address,
+                                &mon_address_longueur);
+
+                continue;
+
+
+            }
+
+            printf("le serveur a recu: %s\n",buffer);
+
+
+
+            sscanf(buffer, "%d %d",&xp, &yp);
+
+
+            res = recherche_tresor(10, x_tresor, y_tresor, xp, yp);
+
+            sprintf(buffer, "%d", res);
+
+            printf("envoyer : %s\n", buffer);
+
+            
+            send(client_socket,buffer, 512, 0);
+
+        }
+
+}
+
+
